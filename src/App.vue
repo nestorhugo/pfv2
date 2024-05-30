6065f30c8759f66aa50a34dca4e884405caabc34
nestorhugo <nestorhugo@proton.me>
2024-05-30 02:21:51 -0300

    added introduction

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,43 @@
   </div>
 
   <div class="p-4 sm:ml-48">
-    <div>
-      <div class="m-5 text-center lg:text-start">
-        <h1 class="text-4xl font-bold text-blue-900">Bem-vindo Usuário!</h1>
-        <p class="text-xl font-bold text-blue-900">
-          Pronto para sua próxima aventura literária?
-        </p>
-      </div>
+    <div class="m-5 text-center lg:text-start">
+      <p class="text-3xl">Hello there! 👋</p>
+      <h1 class="text-5xl md:text-6xl lg:text-7xl font-bold">
+        I’m a designer and developer, and I enjoy combining these two skills to
+        make creative and functional projects.
+      </h1>
 
-      <div class="mt-5 pt-5 ml-5 text-center lg:text-start">
-        <h1 class="text-2xl font-bold text-blue-900">
-          Novidades na biblioteca
-        </h1>
-      </div>
+      <p class="pt-4 text-sm text-gray-400">CURRENTLY</p>
+      <p class="text-2xl">
+        frontend developer @
+        <a href="https://fiqon.com.br/" target="_blank" class="link font-bold"
+          >FiqOn</a
+        >
+      </p>
+      <p class="text-2xl">
+        designer @
+        <a
+          href="https://hipublicidade.net/"
+          target="_blank"
+          class="link font-bold"
+          >Hi Publicidade Digital</a
+        >
+      </p>
+      <p class="text-2xl">
+        internet systems student @
+        <a
+          href="https://www.utfpr.edu.br/"
+          target="_blank"
+          class="link font-bold"
+          >UTFPR</a
+        >
+      </p>
+    </div>
 
-      <div class="p-4 flex flex-wrap justify-center lg:justify-start">
-        <h1>hehe</h1>
-      </div>
+    <div class="m-5 text-center lg:text-start flex justify-between">
+      <p class="pt-4 text-sm text-gray-400">SOME WORKS</p>
+      <a href="/works" class="pt-4 text-sm text-gray-400 link">SEE ALL</a>
     </div>
   </div>
 </template>
