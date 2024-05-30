7741950c3e0d6be25ea290526766225b19024da5
nestorhugo <nestorhugo@proton.me>
2024-05-30 01:15:10 -0300

    ft: added sidebar

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <SideBar />
+  </div>
+
+  <div class="p-4 sm:ml-48">
+    <div>
+      <div class="m-5 text-center lg:text-start">
+        <h1 class="text-4xl font-bold text-blue-900">Bem-vindo Usuário!</h1>
+        <p class="text-xl font-bold text-blue-900">
+          Pronto para sua próxima aventura literária?
+        </p>
+      </div>
+
+      <div class="mt-5 pt-5 ml-5 text-center lg:text-start">
+        <h1 class="text-2xl font-bold text-blue-900">
+          Novidades na biblioteca
+        </h1>
+      </div>
+
+      <div class="p-4 flex flex-wrap justify-center lg:justify-start">
+        <h1>hehe</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBar from "./components/SideBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    SideBar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,105 @@
+<template>
+  <button
+    data-drawer-target="logo-sidebar"
+    data-drawer-toggle="logo-sidebar"
+    aria-controls="logo-sidebar"
+    type="button"
+    class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+  >
+    <span class="sr-only">Open sidebar</span>
+    <svg
+      class="w-6 h-6"
+      aria-hidden="true"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        clip-rule="evenodd"
+        fill-rule="evenodd"
+        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+      ></path>
+    </svg>
+  </button>
+
+  <aside
+    id="logo-sidebar"
+    class="fixed top-0 left-0 z-40 w-48 h-screen transition-transform -translate-x-full sm:translate-x-0"
+    aria-label="Sidebar"
+  >
+    <div
+      class="h-full px-3 py-4 overflow-y-auto bg-white border-r border-gray-200 dark:bg-gray-800"
+    >
+      <a href="/" class="flex items-center ps-2.5 mb-5 pt-5">
+        <img
+          src="./../assets/nestor-logo.svg"
+          class="h-20 me-3 sm:h-20"
+          alt="Nestor Hugo Logo"
+        />
+      </a>
+
+      <ul class="space-y-2 text-2xl pb-5">
+        <li class="link">
+          <a href="/about" class="p-2">about</a>
+        </li>
+        <li class="link">
+          <a href="/works" class="p-2">works</a>
+        </li>
+      </ul>
+
+      <p class="subtext p-2 space-y-2 text-sm text-gray-400">CONTACT</p>
+      <ul class="space-y-2 text-lg pb-5">
+        <li class="link">
+          <a href="mailto:nestorhugo@proton.me" class="p-2">mail</a>
+        </li>
+        <li class="link">
+          <a
+            href="https://www.linkedin.com/in/nestorhugo/"
+            target="_blank"
+            class="p-2"
+            >linkedin</a
+          >
+        </li>
+      </ul>
+
+      <p class="subtext p-2 space-y-2 text-sm text-gray-400">SEE ALSO</p>
+      <ul class="space-y-2 text-lg pb-5">
+        <li class="link">
+          <a href="https://github.com/nestorhugo" target="_blank" class="p-2"
+            >github</a
+          >
+        </li>
+        <li class="link">
+          <a
+            href="https://www.behance.net/nestorhugo"
+            target="_blank"
+            class="p-2"
+            >behance</a
+          >
+        </li>
+      </ul>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style>
+.link {
+  transition: all 0.3s ease;
+  color: black;
+  transform: translateY(0);
+}
+
+.link:hover {
+  color: #606060;
+  transform: translateY(-5px);
+}
+</style>
