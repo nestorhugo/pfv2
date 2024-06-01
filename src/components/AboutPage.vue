beb73c0dc5758b0814c9b7b4cedb767f5fc530d1
nestorhugo <nestorhugo@proton.me>
2024-06-01 02:16:32 -0300

    feat: Update AboutPage with additional content and styling

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-5 text-center lg:text-start">
-    <div class="flex flex-col lg:flex-row items-center">
+    <div class="flex pb-5 flex-col lg:flex-row items-center">
       <div class="lg:w-5/12 pb-5">
         <h1 class="text-3xl md:text-4xl lg:text-5xl mono">
           🖥️ &lt;developer/&gt;
@@ -12,7 +12,7 @@
           🎨 creative
         </h1>
       </div>
-      <div class="lg:w-7/12 flex h-96">
+      <div class="lg:w-7/12 flex">
         <CardWorkGallery
           :source="require('../assets/about/nestor.png')"
           desc="Nestor Hugo"
@@ -32,15 +32,53 @@
         />
       </div>
     </div>
+
+    <div class="flex flex-col lg:flex-row">
+      <div class="lg:w-5/12 pb-5 pr-6">
+        <h1 class="text-4xl font-bold pb-2">About</h1>
+        <p class="text-base md:text-xl pb-5">
+          I'm a developer, designer, and student based in Paraná, Brazil. My
+          passion for technology fuels my constant quest to learn new
+          programming skills. I'm enthusiastic about how technology and
+          programming can transform our world, and I'm dedicated to continually
+          growing in this exciting field.
+        </p>
+
+        <h1 class="text-4xl font-bold pb-2">Design</h1>
+        <p class="text-base md:text-xl pb-5">
+          Since 2019, I've gained over four years of experience in design,
+          working on a variety of projects from social media to corporate
+          design, including creating compelling visual identities. I've had a
+          deep interest in art from a young age and have always channeled my
+          creativity into my work.
+        </p>
+
+        <h1 class="text-4xl font-bold pb-2">Development</h1>
+        <p class="text-base md:text-xl pb-5">
+          In 2021, I started an exciting journey into programming by enrolling
+          at the Federal Technologic University of Paraná. Since then, my
+          passion for technology and coding has grown immensely, leading me to
+          dive deep into web development. In 2023, I began my career as a
+          Fullstack Developer at FiqOn. There, I also got the chance to work as
+          a Software Project Manager on the Systems Integration team. Now, I’m
+          currently working as a Frontend Developer on the product team.
+        </p>
+      </div>
+      <div class="lg:w-7/12">
+        <h1 class="text-4xl font-bold pb-2">Timeline</h1>
+        <TimelineAbout />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import CardWorkGallery from "./CardWorkGallery.vue";
+import TimelineAbout from "./TimelineAbout.vue";
 
 export default {
   name: "AboutPage",
-  components: { CardWorkGallery },
+  components: { CardWorkGallery, TimelineAbout },
 };
 </script>
 
---
+++ src/components/TimelineAbout.vue
@@ -0,0 +1,88 @@
+<template>
+  <ol class="relative border-s border-gray-200 dark:border-gray-700">
+    <li class="mb-10 ms-4">
+      <div
+        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+      ></div>
+      <time
+        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+        >May 2019</time
+      >
+      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+        Designer @ Hi Publicidade Digital
+      </h3>
+      <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+        Social media, corporate design, and visual identities for clients.
+      </p>
+    </li>
+    <li class="mb-10 ms-4">
+      <div
+        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+      ></div>
+      <time
+        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+        >March 2021</time
+      >
+      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+        Started studies @ UTFPR
+      </h3>
+      <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+        Internet Systems Technology course at the Federal Technological
+      </p>
+    </li>
+    <li class="ms-4 mb-10">
+      <div
+        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+      ></div>
+      <time
+        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+        >July 2023</time
+      >
+      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+        Fullstack developer @ FiqOn
+      </h3>
+      <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+        System integrations between APIs. front development using Bootstrap
+      </p>
+    </li>
+    <li class="ms-4 mb-10">
+      <div
+        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+      ></div>
+      <time
+        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+        >March 2024</time
+      >
+      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+        Systems Integration Project Manager @ FiqOn
+      </h3>
+      <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+        System integrations between APIs. front development using Bootstrap
+      </p>
+    </li>
+    <li class="ms-4 mb-10">
+      <div
+        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+      ></div>
+      <time
+        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+        >June 2024</time
+      >
+      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+        Frontend developer @ FiqOn
+      </h3>
+      <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+        Making complex interfaces with Vue.js
+      </p>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: "TimelineAbout",
+  components: {},
+};
+</script>
+
+<style scoped></style>
